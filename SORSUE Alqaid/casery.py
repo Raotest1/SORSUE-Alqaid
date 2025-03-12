ee9ad900fd282516d9bce0d92e1cbf4ab9f78b65
Raotest1 <githabrao@gmail.com>
2025-03-12 09:41:23 +0300

    Uploaded Successfully

---
+++ SORSUE Alqaid/casery.py
@@ -0,0 +1,16 @@
+##مطور السورس
+caes = ["div_victor"]
+casery = "div_victor"
+##ايدي مطور السورس
+caserid = 6363652130
+##قناه السورس
+source = "https://t.me/L9L9J" 
+ch = "L9L9J" 
+##جروب السورس
+group = "https://t.me/L9L9J" 
+##لوجو السورس
+photosource = "https://t.me/xcr1t/5"
+##توكن الصانع
+bot_token="8054346992:AAE_ma2iIagSoCS-u4z7_mEw1go_xk5SvQg"
+##جلسه الاشتراك
+bot_token2="BAFcX10ALH7AEeOGHzSqNqO9kPTu44jp2OadEN6I6b5MG7yA2PLqg3zy8eOjG3jxp9fbm__GWOAhGs9ga16yFc3Fjo7isbAH3cZwO7GUOaVMZ1LvEVN8ay7yLL7sRo9O70KuSYxslCcjPjIDvnwbyNRNFR9paYfLabdIE5oOAP1Ue1Fi9dtSiEalnpvRFHLyri-P-Q0tdi_wZ5V9JBeiGEsvFi9NJATh61Xl1ST3TG6lmRBSJhTGwLnUfDgxZ98PXbqK3cD3oLl3lhQCUdbC90Pf_gGvdtIWTobUpaAaxDyu7UmPdTscD-Y18wN0X-TkxrTKXJDLAlBpIQx5tXl0q-T82i5fbQAAAAGwxrx3AA"
